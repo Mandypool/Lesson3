f5b64ac5ca9a012dff6a56daf8dcc5158fd7974a
Chingis <gonnagowest@mai..ru>
2021-03-13 15:06:02 +0700

    Lesson3

---
+++ ex3.py
@@ -0,0 +1,12 @@
+# Реализовать функцию my_func(), которая принимает три позиционных аргумента и возвращает сумму
+# наибольших двух аргументов.
+
+def my_func(argument_1, argument_2, argument_3):
+    if argument_1 >= argument_3 and argument_2 >= argument_3:
+        return argument_1 + argument_2
+    elif argument_1 > argument_2 and argument_1 < argument_3:
+        return argument_1 + argument_3
+    else:
+        return argument_2 + argument_3
+
+
---
+++ ex7.py
@@ -0,0 +1,15 @@
+# Продолжить работу над заданием. В программу должна попадать строка из слов, разделённых пробелом.
+# Каждое слово состоит из латинских букв в нижнем регистре. Нужно сделать вывод исходной строки,
+# но каждое слово должно начинаться с заглавной буквы. Используйте написанную ранее функцию int_func().
+
+def int_func(a):
+    my_separate = a.split(' ')
+    my_words = []
+    for i in my_separate:
+        letter_elements = str(i)
+        first_letter = letter_elements[:1].upper()
+        word = first_letter + letter_elements[1:]
+        my_words.append(word)
+    return my_words
+
+
